f51006124777bb0a3754e9743c4e09e3bbd2a1f7
Llleea <premont.lea@gmail.com>
2021-03-09 16:31:10 +0100

    Instancier-verser
    Script equation1 :
    pour le moment instancie juste les objets nécessaires à la résolution de la première équation chimique.
    script attaché a la table2
    
    Script verserLiquide :
    attaché sur une fiole, instancie des gouttes en haut de la fiole si on l'oriente pour verser (angle sur x ou z plus grand que 90° ou inférieur à -90°)
    Pas de gestion du temps pour le moment donc ajoute des gouttes à chaque frame

---
+++ Assets/Scripts/verserLiquide.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class verserLiquide : MonoBehaviour
+{
+    public GameObject goutte;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        goutte = Instantiate(goutte, gameObject.transform);
+        goutte.transform.Translate(new Vector3(0.0f, 0.3f, 0.0f));  //A commenter
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //permet de recuperer les donnees de rotation en degré
+        Vector3 rot = gameObject.transform.eulerAngles;
+        
+        //si la rotation suivant x ou z est plus grande(plus petite) que 90(-90), alors on verse du liquide
+        if ((rot.x % 360) > 90 || (rot.x % 360) < -90 || (rot.z % 360) > 90 || (rot.z % 360) < -90)
+        {
+            goutte = Instantiate(goutte, gameObject.transform);
+            //permet de faire apparaitre la goutte à l'entrée de la verrerie
+            goutte.transform.Translate(new Vector3(0.0f, 0.3f, 0.0f));
+        }
+    }
+}
---
+++ Assets/equation1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class equation1 : MonoBehaviour
+{
+    /* Flask1 + Flask2 resoud l'équation et donc l'énigme 1 */
+
+    public GameObject flask1;
+    public GameObject flask2;
+    public GameObject flask3;
+    public GameObject contenant;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject table = gameObject;
+        flask1 = Instantiate(flask1, table.transform);
+        flask1.transform.Translate(new Vector3(0.0f, 1.297f, 0.0f));
+
+        flask2 = Instantiate(flask2, table.transform);
+        flask2.transform.Translate(new Vector3(-0.15f, 1.297f, 0.4f));
+
+        flask3 = Instantiate(flask3, table.transform);
+        flask3.transform.Translate(new Vector3(0.25f, 1.297f, 0.25f));
+
+        contenant = Instantiate(contenant, table.transform);
+        contenant.transform.Translate(new Vector3(0.25f, 1.297f, -0.5f));
+
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
